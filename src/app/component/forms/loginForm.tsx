b0357e82f1ba5370b6e9eb64a36a959be9f4b2ac
ruhgm <ruhamaasstu@gmail.com>
2024-05-01 05:29:35 -0900

    update

--- src/app/component/forms/loginForm.tsx
+++ src/app/component/forms/loginForm.tsx
@@ -72,9 +72,7 @@ export default function LoginForm() {
         <p className="mt-10 text-center text-sm text-gray-500">
           Don't have an account?{' '}
           <Link href="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-           
               Sign up
-          
           </Link>
         </p>
       </div>
