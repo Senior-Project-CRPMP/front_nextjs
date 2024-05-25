9d0e3c824ab5614dbaac63c75efa46388d9136c9
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2024-05-25 11:00:29 +0300

    form feature updates

---
+++ src/app/project_dashboard/Form/DeleteForm/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { useRouter } from 'next/navigation';
+
+const DeleteForm: React.FC = () => {
+    return (
+       <>
+         <h1>hello</h1>
+       </> 
+    )
+}
+
+export default DeleteForm
---
+++ src/app/project_dashboard/Form/UpdateForm/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { useRouter } from 'next/navigation';
+
+const UpdateForm: React.FC = () => {
+    return (
+       <>
+         <h1>hello</h1>
+       </> 
+    )
+}
+
+export default UpdateForm
