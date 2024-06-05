d24e61d2009c9ab60800297e5f98c99c76a11da3
Meron-kedir <88824826+Meron-kedir@users.noreply.github.com>
2024-06-05 08:31:28 +0300

    updated account

--- src/app/dashboard/account/page.tsx
+++ src/app/dashboard/account/page.tsx
@@ -1,116 +1,52 @@
-import { PaperClipIcon } from "@heroicons/react/20/solid";
+"use client";
+import React, { useState } from "react";
+import Navigation from "./navigation";
+import ProfilePage from "./profileAndVisibility";
+import EmailPage from "./email";
+import SecurityPage from "./SecurityContent";
+import AccountPreferencesPage from "./accountPreferences";
 
-export default function Account() {
+const AccountPage: React.FC = () => {
+  const [selectedNavItem, setSelectedNavItem] = useState(
+    "Profile and visibility"
+  );
+
+  const renderContent = () => {
+    switch (selectedNavItem) {
+      case "Profile and visibility":
+        return <ProfilePage />;
+      case "Email":
+        return <EmailPage />;
+      case "Security":
+        return <SecurityPage />;
+      case "Account preferences":
+        return <AccountPreferencesPage />;
+      case "Connected APPS":
+        return <div>Connected APPS content</div>;
+      case "Link preferences":
+        return <div>Link preferences content</div>;
+      case "Product settings":
+        return <div>Product settings content</div>;
+      default:
+        return <div>Select an option</div>;
+    }
+  };
+
   return (
-    <div className="h-full bg-white p-5">
-      <div className="px-4 sm:px-0">
-        <h3 className="text-base font-semibold leading-7 text-gray-900">
-          Account Information
-        </h3>
-        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-          Personal details and application.
-        </p>
-      </div>
-      <div className="mt-6 border-t border-gray-100">
-        <dl className="divide-y divide-gray-100">
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Full name
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              Meron Kedir
-            </dd>
+    <div>
+      <div className="flex space-x-2">
+        <div className="h-screen bg-white rounded-md  mr-2 w-4/5">
+          <div className="p-4 flex-col h-screen space-x-0">
+            <div className="h-1/6">
+              <Navigation onNavItemSelect={setSelectedNavItem} />
+              <div className="border-t border-grey"></div>
+            </div>
+            <div className="h-5/6">{renderContent()}</div>
           </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Status
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              Frontend Developer
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Email address
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              merryked@gmail.com
-            </dd>
-          </div>
-          {/* <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Salary expectation
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              $120,000
-            </dd>
-          </div> */}
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              About
-            </dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              A passionate developer currently working on research project.
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">
-              Attachments
-            </dt>
-            <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul
-                role="list"
-                className="divide-y divide-gray-100 rounded-md border border-gray-200"
-              >
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon
-                      className="h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">
-                        Research 1.pdf
-                      </span>
-                      <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Download
-                    </a>
-                  </div>
-                </li>
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon
-                      className="h-5 w-5 flex-shrink-0 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">
-                        Research 2.pdf
-                      </span>
-                      <span className="flex-shrink-0 text-gray-400">4.5mb</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a
-                      href="#"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      Download
-                    </a>
-                  </div>
-                </li>
-              </ul>
-            </dd>
-          </div>
-        </dl>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default AccountPage;
